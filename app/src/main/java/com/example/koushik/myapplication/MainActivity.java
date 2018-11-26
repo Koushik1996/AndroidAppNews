39906e44a50eaf5ccfd23f3c9c81f6bb4c612487
Koushik1996 <koushik.rjn@gmail.com>
2018-11-26 22:10:09 +0530

    Initial

---
+++ app/src/main/java/com/example/koushik/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.koushik.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    private int flag;
+    String a;
+    MyData M;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        flag = 0;
+        TextView myFirstView;
+        myFirstView = findViewById(R.id.textView);
+        a = (String) myFirstView.getText();
+        String yo = "Hey Bitch";
+        M = new MyData();
+        M.setA(yo);
+    }
+    public void myfirstclick(View v)
+    {
+        TextView myFirstView;
+        myFirstView = findViewById(R.id.textView);
+        if(flag == 0)
+        {
+            myFirstView.setText(M.getA());
+            flag = 1 ;
+        }
+        else
+        {
+            myFirstView.setText(a);
+            flag = 0;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/koushik/myapplication/MyData.java
@@ -0,0 +1,12 @@
+package com.example.koushik.myapplication;
+public class MyData {
+    String A;
+    public void setA(String S)
+    {
+        A = S;
+    }
+    public String getA()
+    {
+        return A;
+    }
+}
