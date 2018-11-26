7acf1a7bbab45f0f7dd46da8e3fcc4405c815ef2
Koushik1996 <koushik.rjn@gmail.com>
2018-11-26 23:18:48 +0530

    Initial_without_androidTest

--- app/src/main/java/com/example/koushik/myapplication/MainActivity.java
+++ app/src/main/java/com/example/koushik/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.koushik.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("ksk","HEy Creating the activity");
         setContentView(R.layout.activity_main);
         flag = 0;
         TextView myFirstView;
@@ -20,7 +23,41 @@ public class MainActivity extends AppCompatActivity {
         M = new MyData();
         M.setA(yo);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ksk","HEy Starting the activity");
+    }
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.i("ksk","HEy ReStarting the activity");
+    }
+    @Override
+    protected void onPause()
+    {
+        Log.i("ksk","HEy Pausing the activity");
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop()
+    {
+        Log.i("ksk","HEy Stoping the activity");
+        super.onStop();
+
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i("ksk","HEy Destroying the activity");
+    }
     public void myfirstclick(View v)
     {
-
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
 }
